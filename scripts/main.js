16fd999784337e83c52f83f83bd5f90630ab6bf2
bag3318 <bglatman@outlook.com>
2016-12-08 11:02:08 -0800

    Update style effects

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,19 @@
 // jQuery Document			
-$(document).ready(function() {
-	$("#accordion").accordion({
-		collapsible: true,
-		heightStyle: "content",
-		active: false
-	});
-	$("#tabs").tabs({
-		collapsible: true,
-		heightStyle: "fill"
-	});
+$(document).ready(function () {
+
 	$("#btn").button();
+	$("#btn1").button();
+	$("#btn1").on("click", function () {
+		$("#start").fadeOut(888);
+		$("#accordion").fadeIn(888); // or 300
+		$("#accordion").accordion({
+			collapsible: true,
+			heightStyle: "content",
+			active: false
+		});
+		$("#tabs").tabs({
+			collapsible: true,
+			heightStyle: "fill"
+		});
+	});
 });
