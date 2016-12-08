6ded38222a59a4be16839024b3d9d201dde548f8
bag3318 <bglatman@outlook.com>
2016-12-08 10:09:57 -0800

    Add reload scripts

---
+++ scripts/reload.js
@@ -0,0 +1,19 @@
+// JavaScript Document
+var pageReload = (function(){
+	function reload() {
+		window.location.reload(true);
+	}
+	function init() {
+		var element = document.querySelector("#btn");
+		element.addEventListener("click", function() {
+			var script = pageReload();
+			script.reload();
+		});
+	}
+	return function returnAPI(api) {
+		return {
+			init: init,
+			reload: reload
+		};
+	};
+}());
