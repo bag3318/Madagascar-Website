d1cfa61930de8f2ce9b615597e1b36ea7cf2b276
bag3318 <bglatman@outlook.com>
2016-12-12 12:45:40 -0800

    Add comments

--- scripts/scrolling_title.js
+++ scripts/scrolling_title.js
@@ -1,10 +1,10 @@
 // JavaScript Document
-var scroller = (function titleScroller(text) {
-    "use strict";
-	document.title = text;
-    setTimeout(function () {
-        titleScroller(text.substr(1) + text.substr(0, 1));
-    }, 88); 
+var scroller = (function titleScroller(text) { // define a master function set to a global variable; pass in the text parameter
+    "use strict"; // make sure to use strict mode
+	document.title = text; // make the html documents title = to the text parameter of the master function
+    setTimeout(function () { // set the timeout
+        titleScroller(text.substr(1) + text.substr(0, 1)); 
+    }, 88);  // 88 milliseconds between timeouts
 	function init() {
 		var element = window;
 		element.addEventListener("load", function() {
